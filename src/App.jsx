dc6438dddb73ee43142a43f51c53687c8183525f
Persona743 <siyavashshahidi7@gmail.com>
2023-03-11 19:47:51 +0330

    allNewDice func added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import Die from './components/Die'
+import Die from './components/Die';
 
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
 export default function App() {
+    const [dice, setDice] = useState(allNewDice());
 
-  return (
-      <main>
-          <div className='dice-container'>
-              <Die value="1" />
-              <Die value="2" />
-              <Die value="3" />
-              <Die value="4" />
-              <Die value="5" />
-              <Die value="6" />
-              <Die value="1" />
-              <Die value="1" />
-              <Die value="1" />
-              <Die value="1" />
-          </div>
-      </main>
-  );
+    function allNewDice() {
+        const newDice = [];
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6));
+        }
+        return newDice;
+    }
+
+    const diceElements = dice.map((die) => <Die value={die} />);
+
+    return (
+        <main>
+            <div className="dice-container">{diceElements}</div>
+        </main>
+    );
 }
