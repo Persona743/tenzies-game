54653c01c994ac2666780c8f74f6517e67bd55f4
Persona743 <siyavashshahidi7@gmail.com>
2023-03-12 01:28:15 +0330

    conditional style added

--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function die(props) {
-  return (
-      <div className='die-face'>
-          <h2 className='die-num'>{ props.value}</h2>
-    </div>
-  )
+    const styles = {
+        backgroundColor: props.isHeld ? '#59E391' : 'white',
+    };
+
+    return (
+        <div className="die-face" style={styles}>
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    );
 }
