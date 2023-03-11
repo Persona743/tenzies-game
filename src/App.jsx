6bef6bf871505282ed4f90fcba4ada579daf5e21
Persona743 <siyavashshahidi7@gmail.com>
2023-03-11 19:19:02 +0330

    die component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import Die from './components/Die'
+
 import './App.css'
 
 export default function App() {
 
   return (
-    <main>
-     
-    </main>
-  )
+      <main>
+          <div className='dice-container'>
+              <Die value="1" />
+              <Die value="2" />
+              <Die value="3" />
+              <Die value="4" />
+              <Die value="5" />
+              <Die value="6" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+          </div>
+      </main>
+  );
 }
