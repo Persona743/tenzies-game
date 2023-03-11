a533361da4e2bd32e5e28312d6afcdeef0cddd60
Persona743 <siyavashshahidi7@gmail.com>
2023-03-11 23:47:51 +0330

    nanoid added

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,36 @@ import Die from './components/Die';
 import './App.css';
 import { useState } from 'react';
 
+import { nanoid } from 'nanoid';
+
 export default function App() {
     const [dice, setDice] = useState(allNewDice());
 
     function allNewDice() {
         const newDice = [];
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6));
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid(),
+            });
         }
         return newDice;
     }
-  
-  function rollDice() {
-    setDice(allNewDice())
-  }
 
-    const diceElements = dice.map((die) => <Die value={die} />);
+    function rollDice() {
+        setDice(allNewDice());
+    }
+
+  const diceElements = dice.map((die) =>
+    <Die key={die.id} value={die.value} />);
 
     return (
         <main>
-        <div className="dice-container">{diceElements}</div>
-        <button className='roll-dice-btn' onClick={rollDice}>Roll</button>
+            <div className="dice-container">{diceElements}</div>
+            <button className="roll-dice-btn" onClick={rollDice}>
+                Roll
+            </button>
         </main>
     );
 }
