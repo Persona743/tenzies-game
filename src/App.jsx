9091477d51fe1ec84008d44455212cad83b3617d
Persona743 <siyavashshahidi7@gmail.com>
2023-03-11 19:56:56 +0330

    roll btn added

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,17 @@ export default function App() {
         }
         return newDice;
     }
+  
+  function rollDice() {
+    setDice(allNewDice())
+  }
 
     const diceElements = dice.map((die) => <Die value={die} />);
 
     return (
         <main>
-            <div className="dice-container">{diceElements}</div>
+        <div className="dice-container">{diceElements}</div>
+        <button className='roll-dice-btn' onClick={rollDice}>Roll</button>
         </main>
     );
 }
