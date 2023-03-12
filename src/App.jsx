88a98786c0bc174663ddf7a64c383490c3758b8d
Persona743 <siyavashshahidi7@gmail.com>
2023-03-12 16:50:32 +0330

    confetti added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 export default function App() {
     const [dice, setDice] = useState(allNewDice());
@@ -15,7 +16,6 @@ export default function App() {
         const allSameValue = dice.every((die) => die.value === firstValue);
         if (allHeld && allSameValue) {
             setTenzies(true);
-            console.log('You Won');
         }
     }, [dice]);
 
@@ -37,11 +37,16 @@ export default function App() {
     }
 
     function rollDice() {
-        setDice((oldDice) =>
-            oldDice.map((die) => {
-                return die.isHeld ? die : generateNewDie();
-            })
-        );
+        if (!tenzies) {
+            setDice((oldDice) =>
+                oldDice.map((die) => {
+                    return die.isHeld ? die : generateNewDie();
+                })
+            );
+        } else {
+            setTenzies(false);
+            setDice(allNewDice());
+        }
     }
 
     function holdDice(id) {
@@ -63,6 +68,7 @@ export default function App() {
 
     return (
         <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">
                 Roll until all dice are the same. Click each die to freeze it at
